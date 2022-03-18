37fdf625cf2b5320d19dfab60b9c07b7f1be4cf6
jose carlos <jose1196@hotmail.com>
2022-03-18 20:14:26 +0100

    creado componentes Login, home, rec_pass, registro

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { RecuperarPasswordComponent } from './recuperar-password/recuperar-password.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegistroComponent,
+    RecuperarPasswordComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
