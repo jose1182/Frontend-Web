c3ffde360224150ee92684c849b5a6a319a53663
anrogo <antoniorogo96@gmail.com>
2022-05-04 18:37:35 +0200

    contratos con login

--- src/app/components/contratos/contratos.component.ts
+++ src/app/components/contratos/contratos.component.ts
@@ -2,6 +2,8 @@ import { ContratoModel } from './../../model/contrato.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ContratosService } from 'src/app/services/contratos/contratos.service';
+import { AccountModel } from 'src/app/model/account.model';
+import { AccountService } from 'src/app/services/account.service';
 
 @Component({
   selector: 'app-contratos',
@@ -11,19 +13,42 @@ import { ContratosService } from 'src/app/services/contratos/contratos.service';
 export class ContratosComponent implements OnInit {
 
   id!: number;
-  contratos!: ContratoModel[]; 
+  contratos!: ContratoModel[];
+  accountModel!: AccountModel | null;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private contratosService: ContratosService
+    private contratosService: ContratosService,
+    private accountService: AccountService
   ) { }
 
   ngOnInit(): void {
+    this.checkLogin();
+  }
+
+  goToViewDetail(id: Number): void{
+    this.router.navigate(['contrato', id]);
+  }
+
+  checkLogin(): void {
+    this.accountService.identify(true).subscribe( account => {
+      if(account){
+        this.accountModel = account
+        console.log("Logueado para ver los contratos");
+        this.getContratosList();
+      } else {
+        console.log('No has iniciado sesión');
+        this.router.navigate(['home']);
+      }
+    })
+  }
+
+  getContratosList(): void {
     this.route.paramMap.subscribe((params: Params) => {
       if(params.get('id')){
         this.id = params.get('id');
-        console.log(this.id);
+        //console.log(this.id);
 
         this.contratosService.getContratoByUserId(this.id).subscribe(contratos => {
           if(contratos){
@@ -33,10 +58,6 @@ export class ContratosComponent implements OnInit {
         })
       }
     })
-  }
-
-  goToViewDetail(id: Number): void{
-    this.router.navigate(['contrato', id]);
   }
 
 }
--- src/app/model/contrato.model.ts
+++ src/app/model/contrato.model.ts
@@ -1,11 +1,18 @@
 import { ServicioModel } from './servicio.model';
 import { Usuario } from "./usuario.model";
 
-
 export class ContratoModel {
     id!: Number;
     preciofinal!: Number;
     fecha!: String;
     usuario!: Usuario;
     servicio!: ServicioModel;
+
+    constructor(id: Number, preciofinal: Number, fecha: String, usuario: Usuario, servicio: ServicioModel){
+        this.id = id;
+        this.preciofinal = preciofinal;
+        this.fecha = fecha;
+        this.usuario = usuario;
+        this.servicio = servicio;
+    }
 }
