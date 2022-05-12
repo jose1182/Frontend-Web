fbfc1c8a42f562de3e13cc9267d8c481ee871bf5
jose carlos <jose1196@hotmail.com>
2022-05-13 00:14:49 +0200

    componente toas crear controto ok, redirecionmaito okcreado

--- src/app/model/contrato.model.ts
+++ src/app/model/contrato.model.ts
@@ -1,18 +1,25 @@
-import { Usuario } from "./usuario.model";
+import { Usuario, IUsuario } from './usuario.model';
 import * as dayjs from 'dayjs';
 import { IServicio } from "./servicio.model";
-export class ContratoModel {
-    id!: number;
-    preciofinal!: number;
-    fecha!: dayjs.Dayjs;
-    usuario!: Usuario;
-    servicio!: IServicio;
 
-    constructor(id: number, preciofinal: number, fecha: dayjs.Dayjs, usuario: Usuario, servicio: IServicio) {
-        this.id = id;
-        this.preciofinal = preciofinal;
-        this.fecha = fecha;
-        this.usuario = usuario;
-        this.servicio = servicio;
-    }
+export interface IContrato {
+  id?: number;
+  preciofinal?: number;
+  fecha?: dayjs.Dayjs;
+  usuario?: IUsuario | null;
+  servicio?: IServicio | null;
+}
+
+export class Contrato implements IContrato {
+  constructor(
+    public id?: number,
+    public preciofinal?: number,
+    public fecha?: dayjs.Dayjs,
+    public usuario?: IUsuario | null,
+    public servicio?: IServicio | null
+  ) {}
+}
+
+export function getContratoIdentifier(contrato: IContrato): number | undefined {
+  return contrato.id;
 }
--- src/app/services/contratos/contratos.service.ts
+++ src/app/services/contratos/contratos.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { ContratoModel } from '../../model/contrato.model';
+import { IContrato } from '../../model/contrato.model';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +13,29 @@ export class ContratosService {
     private http: HttpClient
   ) { }
 
-  getContratoById(id: number | undefined):Observable<ContratoModel>{
-    return this.http.get<ContratoModel>(`${environment.url}contratoes/${id}`);
+  getContratoById(id: number | undefined):Observable<IContrato>{
+    return this.http.get<IContrato>(`${environment.url}contratoes/${id}`);
   }
 
-  getContratoByUserId(id: number | undefined):Observable<ContratoModel[]>{
-    return this.http.get<ContratoModel[]>(`${environment.url}contratoes/usuario/?id=${id}`);
+  getContratoByUserId(id: number | undefined):Observable<IContrato[]>{
+    return this.http.get<IContrato[]>(`${environment.url}contratoes/usuario/?id=${id}`);
+  }
+
+
+  getContratoByServiceId(contrato: IContrato): Observable<IContrato[]>{
+    let params = new HttpParams()
+    params = params.append("usuarioId.equals", contrato.usuario?.id as number);
+    params = params.append("servicioId.equals", contrato.servicio?.id as number)
+    //params = params.append("id.in", busqueda.id.in.join(', '))
+    //params = params.append("servicioId", "")
+
+
+    return this.http.get<IContrato[]>(environment.url + "contratoes", {params: params}).pipe(map(result => {
+      return result;
+    }));
+  }
+
+  create(contrato: IContrato): Observable<IContrato>{
+    return this.http.post<IContrato>(environment.url + "contratoes", contrato)
   }
 }
